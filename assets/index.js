f6d3584b6999ed1b375141a2c8ba03f0f743fd84
ogkomasic <szymon.szulczynski44@gmail.com>
2025-03-30 00:01:06 +0100

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -57,7 +57,7 @@ imageInput.addEventListener('change', (event) => {
     fetch('	https://api.imgur.com/3/image' ,{
         method: 'POST',
         headers: {
-            'Authorization': 'Client-ID c8c28d402435402'
+            'Authorization': 'Client-ID 210232625f5781d'
         },
         body: data
     })
