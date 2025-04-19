190907ccae429ff86d07e126db4b81aa92681bbd
ogkomasic <szymon.szulczynski44@gmail.com>
2025-04-20 01:01:33 +0200

    Add files via upload

--- assets/index.js
+++ assets/index.js
@@ -57,7 +57,7 @@ imageInput.addEventListener('change', (event) => {
     fetch('	https://api.imgur.com/3/image' ,{
         method: 'POST',
         headers: {
-            'Authorization': 'Client-ID 210232625f5781d'
+            'Authorization': 'Client-ID c8c28d402435402'
         },
         body: data
     })
@@ -139,7 +139,7 @@ function isEmpty(value){
 
 function forwardToId(params){
 
-    location.href = "id?" + params
+    location.href = "id.html?" + params;
 
 }
 
